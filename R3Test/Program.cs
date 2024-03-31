0b8c1ee546907a52516722058fb4b84381b5d42b
ongaeshi <ongaeshi0621@gmail.com>
2024-04-01 00:36:16 +0900

    feat: サンプル整理

--- R3Test/Program.cs
+++ R3Test/Program.cs
@@ -1,14 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Data;
 using System.Diagnostics.CodeAnalysis;
 using R3;
 
-var numbers = new MySequenceOfNumbers();
-var observer = new MyConsoleObserver<int>();
-numbers.Subscribe(observer);
+//Sample1();
+//Sample2();
+ReplaySubjectWindowExample();
 
+void Sample1()
+{
+    var numbers = new MySequenceOfNumbers();
+    var observer = new MyConsoleObserver<int>();
+    numbers.Subscribe(observer);
+}
 
-public class MyConsoleObserver<T> : Observer<T>
+void Sample2()
+{
+    var subject = new Subject<string>();
+    // var subject = new ReplaySubject<string>();
+    WriteSequenceToConsole(subject);
+    subject.OnNext("a");
+    subject.OnNext("b");
+    subject.OnNext("c");
+    Console.ReadKey();
+}
+
+void WriteSequenceToConsole(Observable<string> sequence)
+{
+    sequence.Subscribe(Console.WriteLine);
+}
+
+void ReplaySubjectWindowExample()
+{
+    var window = TimeSpan.FromMilliseconds(150);
+    var subject = new ReplaySubject<string>(window);
+    subject.OnNext("w");
+    Thread.Sleep(TimeSpan.FromMilliseconds(100));
+    subject.OnNext("x");
+    Thread.Sleep(TimeSpan.FromMilliseconds(100));
+    subject.OnNext("y");
+    subject.Subscribe(Console.WriteLine);
+    subject.OnNext("z");
+}
+
+class MyConsoleObserver<T> : Observer<T>
 {
     protected override void OnNextCore(T value) => Console‌.WriteLine("Received value {0}", value);
     protected override void OnErrorResumeCore(Exception error) => Console‌.WriteLine("Sequence faulted with {0}", error);
@@ -27,3 +63,6 @@ class MySequenceOfNumbers : Observable<int>
     }
 }
 
+// -------------------------------------------
+
+// -------------------------------------------
